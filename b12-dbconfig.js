5256e00f72cc138be7d64f2d66782051c8864151
Suttawee Chatpattanasiri <98304076+net253@users.noreply.github.com>
2022-01-24 13:39:11 +0700

    Add files via upload

---
+++ b12-dbconfig.js
@@ -0,0 +1,15 @@
+const config = {
+  user: "sa-b12",
+  // user: "sa",
+  password: "Welc0me@2021",
+  // password: "12345678",
+  server: "10.0.0.4",
+  // server: "127.0.0.1",
+  database: "IoT_b12",
+  options: {
+    encrypt: false,
+    trustServerCertificate: false,
+  },
+};
+
+module.exports = config;
